093c954cbb02f3dcc25925bd8980c31f8883ee6e
Constantin Brindusoiu <BrandConstantin@users.noreply.github.com>
2020-11-10 23:43:57 +0100

    create project with spring initializr

---
+++ treasuryDemo/src/main/java/com/springboot/rest/exercise/treasuryDemo/TreasuryDemoApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.rest.exercise.treasuryDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreasuryDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreasuryDemoApplication.class, args);
+	}
+
+}
